686904b43078b44edfc51ec8572a5aaacead5353
Jose Bacilio <74934503@continental.edu.pe>
2025-10-18 04:11:37 -0500

    cambios

--- sistema_infraccion/settings.py
+++ sistema_infraccion/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'sistema_infraccion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -93,6 +93,7 @@ DATABASES = {
         },
     }
 }
+
 
 
 # Password validation
--- sistema_infraccion/urls.py
+++ sistema_infraccion/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for sistema_infraccion project.
+URL configuration for seguridad project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", lambda request: redirect("dashboard_home")),  
+    path("admin/", admin.site.urls),
+    path("dashboard/", include("dashboard.urls")),
+    path("camaras/", include("camaras.urls")),
+    path("infracciones/", include("infracciones.urls")),
+    path("ml-predicciones/", include("ml_predicciones.urls")),
+    path("api/", include("api.urls")),
 ]
+    
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
